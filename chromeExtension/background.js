8ebf123d9c25accfac7993e69ea2acb188f6cdac
Dominik Schmid <DominikSchmid93@gmail.com>
2015-11-28 22:28:59 +1300

    Redirected the user to the reader when the extension button is clicked

---
+++ chromeExtension/background.js
@@ -0,0 +1,18 @@
+
+// point the browser at the ReadHomestuck page
+function openReadHomestuckPage() {
+    chrome.tabs.create({
+        url: "http://DomNomNom.com/ReadHomestuck/",
+        active: true,
+    });
+}
+
+// execute the above when the extension's icon is clicked
+chrome.browserAction.onClicked.addListener(openReadHomestuckPage);
+
+// execute on install (but not update)
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === "install") {
+        openReadHomestuckPage();
+    }
+});
